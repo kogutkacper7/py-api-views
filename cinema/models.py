397076dc87f07d8b2b40454c5fef8e548e35e917
Kacper <kogutkacper7@gmail.com>
2026-05-06 14:51:25 +0200

    fix: add unique constraint to Genre name

--- cinema/models.py
+++ cinema/models.py
@@ -7,7 +7,7 @@ class Actor(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
 
 class CinemaHall(models.Model):
